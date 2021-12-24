4056fd04a20ec433b5defcbddfa8f84f0d034e12
주성호 <etkl8888@naver.com>
2021-12-24 23:00:36 +0900

    Initial commit

---
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -0,0 +1,17 @@
+package hello.servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+
+// 서블릿을 자동으로 등록해줌
+@ServletComponentScan
+@SpringBootApplication
+public class ServletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServletApplication.class, args);
+	}
+
+}
