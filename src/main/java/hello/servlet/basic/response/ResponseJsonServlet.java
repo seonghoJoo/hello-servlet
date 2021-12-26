4238e5c481df08154e90ae6acfacd6323ef37992
주성호 <etkl8888@naver.com>
2021-12-26 21:21:52 +0900

    ADD Member Servlet

--- src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
+++ src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 
 @WebServlet(name="responseJsonServlet", urlPatterns = "/response-json")
 public class ResponseJsonServlet extends HttpServlet {
@@ -19,11 +20,20 @@ public class ResponseJsonServlet extends HttpServlet {
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("application/json");
         resp.setCharacterEncoding("utf-8");
-        HelloData helloData = new HelloData();
-        helloData.setAge(20);
-        helloData.setUsername("joo");
+        ArrayList<HelloData> list = new ArrayList<>();
 
-        String result = objectMapper.writeValueAsString(helloData);
+        for(int i=0;i<20;i++){
+            HelloData helloData = new HelloData();
+            helloData.setAge(20);
+            char c = (char)('a'+ i);
+            helloData.setUsername(c+"주성호");
+            list.add(helloData);
+        }
+        for(HelloData c : list){
+            System.out.println(c.getUsername());
+        }
+
+        String result = objectMapper.writeValueAsString(list);
         //{"username":"joo", "age":20}
         resp.getWriter().println(result);
     }
